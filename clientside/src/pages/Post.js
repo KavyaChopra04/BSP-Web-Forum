9e099e2129e36e6afbfc7859bd5d3589d17e3b6c
Kavya <chopra.kavya04@gmail.com>
2022-07-08 13:11:41 +0530

    image support

--- clientside/src/pages/Post.js
+++ clientside/src/pages/Post.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import { post } from '../../../serverside/routes/Posts';
 function Post() {
 let {id} =useParams();
 const [username, setUsername]=useState("");
